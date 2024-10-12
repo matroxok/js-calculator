301f24e7dbe4397892ae3c279e60fdf0f1fdfe8a
matroxok <matrox.biznesyt@gmail.com>
2024-10-12 17:23:15 +0200

    working at desktop version

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,29 @@
 import Input, { Years, Procent } from './resources/tabele'
 import Option, { Button } from './resources/options'
-import Footer, { CalculatorIcon } from './resources/footer'
+import Footer, { CalculatorIcon, FooterDesktop } from './resources/footer'
 
 const App = () => {
 	return (
 		<>
-			<div className="p-5">
-				<h1 className="font-Jakarta">Mortage Calculator</h1>
-				<h2 className="font-Jakarta">Clear All</h2>
-				<Input></Input>
-				<Years />
-				<Procent></Procent>
-				<Option></Option>
-				<Button />
+			<div className="md:w-screen md:h-screen md:bg-blue-100 md:flex md:justify-center md:items-center">
+				<div className="md:flex md:bg-white md:w-auto md:h-auto md:rounded-3xl overflow-hidden">
+					<div className="p-5 md:p-10 md:w-auto md:h-auto ">
+						<div className="md:flex md:w-auto md:gap-52 items-center">
+							<h1 className="font-Jakarta font-bold text-2xl ">Mortage Calculator</h1>
+							<h2 className="font-Jakarta font-thin underline underline-offset-1 text-gray-500">Clear All</h2>
+						</div>
+						<Input></Input>
+						<div className="hidden md:flex w-full h-auto gap-8">
+							<Years />
+							<Procent></Procent>
+						</div>
+						<Option></Option>
+						<Button />
+					</div>
+					<FooterDesktop></FooterDesktop>
+					<Footer></Footer>
+				</div>
 			</div>
-			<Footer></Footer>
 		</>
 	)
 }
--- src/resources/tabele.jsx
+++ src/resources/tabele.jsx
@@ -3,7 +3,7 @@ import CurrencyPoundIcon from '@mui/icons-material/CurrencyPound'
 const Inputs = () => {
 	return (
 		<div className="mt-5">
-			<p>Lorem, ipsum.</p>
+			<p>Mortage Amount</p>
 			<div className="mt-1 border border-gray-500 rounded">
 				<div className="w-10 h-10 bg-blue-200 flex justify-center items-center">
 					<CurrencyPoundIcon fontSize="inherit" />
@@ -15,9 +15,9 @@ const Inputs = () => {
 
 export const Years = () => {
 	return (
-		<div className="mt-5">
+		<div className="mt-5 w-1/2">
 			<p>Lorem, ipsum.</p>
-			<div className="mt-1 border border-gray-500 rounded flex flex-row-reverse">
+			<div className="mt-1 border border-gray-500 rounded flex flex-row-reverse ">
 				<div className="w-14 h-10 bg-blue-200 flex justify-center items-center">
 					<p className="font-bold text-sm">years</p>
 				</div>
@@ -28,7 +28,7 @@ export const Years = () => {
 
 export const Procent = () => {
 	return (
-		<div className="mt-5">
+		<div className="mt-5 w-1/2">
 			<p>Lorem, ipsum.</p>
 			<div className="mt-1 border border-gray-500 rounded flex flex-row-reverse">
 				<div className="w-10 h-10 bg-blue-200 flex justify-center items-center">
