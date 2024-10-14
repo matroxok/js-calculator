83160ab98fc15edd2446d04bfb04d5ac9fa58500
matroxok <matrox.biznesyt@gmail.com>
2024-10-14 21:21:24 +0200

    added inputs

--- src/resources/tabele.jsx
+++ src/resources/tabele.jsx
@@ -4,10 +4,11 @@ const Inputs = () => {
 	return (
 		<div className="mt-5 md:mt-8 ">
 			<p>Mortage Amount</p>
-			<div className="mt-1 md:mt-4 border border-gray-500 rounded">
+			<div className="mt-1 md:mt-4 border border-gray-500 rounded flex items-center">
 				<div className="w-10 h-10 bg-blue-200 flex justify-center items-center">
 					<CurrencyPoundIcon fontSize="inherit" />
 				</div>
+				<input type="number" className="w-full h-10 px-3 focus:outline-none" placeholder="Enter amount" />
 			</div>
 		</div>
 	)
@@ -21,6 +22,7 @@ export const Years = () => {
 				<div className="w-14 h-10 bg-blue-200 flex justify-center items-center">
 					<p className="font-bold text-sm">years</p>
 				</div>
+				<input type="number" className="w-full h-10 px-3 focus:outline-none" placeholder="Enter amount" />
 			</div>
 		</div>
 	)
@@ -34,6 +36,7 @@ export const Procent = () => {
 				<div className="w-10 h-10 bg-blue-200 flex justify-center items-center">
 					<p className="font-bold text-sm">%</p>
 				</div>
+				<input type="number" className="w-full h-10 px-3 focus:outline-none" placeholder="Enter amount" />
 			</div>
 		</div>
 	)
